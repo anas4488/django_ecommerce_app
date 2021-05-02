162b53cc4b1903cbbb05a693a72afb16c36a682e
anas4488 <anassayed63@gmail.com>
2021-05-02 23:57:07 +0530

    Second commit : creating models and handling them in admin page. Also created views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
+    products = Product.objects.all()
     context = {}
     return render(request, 'store/store.html', context)
 
