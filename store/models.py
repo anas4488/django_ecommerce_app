a8e608a40c06e84ad19bf01f9340ef2f7a611896
anas4488 <anassayed63@gmail.com>
2021-05-02 23:45:37 +0530

    Second commit : creating models and handling them in admin page.

--- store/models.py
+++ store/models.py
@@ -30,4 +30,19 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
-    
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete = models.SET_NULL, blank=True, null=True )
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    pincode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
